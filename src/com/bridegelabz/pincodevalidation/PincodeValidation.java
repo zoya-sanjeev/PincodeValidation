dbfb8cd6a7e3d7a98cb74c032570bf3861c0fd80
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-07 23:38:04 +0530

    ADDED: pincodeValidation method to check for 6 digit pincode

--- src/com/bridegelabz/pincodevalidation/PincodeValidation.java
+++ src/com/bridegelabz/pincodevalidation/PincodeValidation.java
@@ -1,9 +1,22 @@
 package com.bridegelabz.pincodevalidation;
 
+import java.util.Scanner;
+import java.util.regex.*;
 public class PincodeValidation {
+	
+	public static boolean pincodeValidation(String pincode) {
+		String pincodeRegex = "^[0-9]{6}$";
+		return Pattern.matches(pincodeRegex, pincode);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter pincode");
+		String pincode=scanner.nextLine();
+		while(!pincodeValidation(pincode)) {
+			System.out.println("Invalid pincode.Enter again");
+			pincode=scanner.nextLine();
+		}
 
 	}
 
